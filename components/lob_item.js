8cd552562d6e62228aad842c21440f3cb7561c71
peccu <peccul@gmail.com>
2019-09-21 02:33:43 +0900

    extract item rendering

---
+++ components/lob_item.js
@@ -0,0 +1,90 @@
+(function fragments() {
+
+  Sqrl.defineFilter('encodeURIComponent', encodeURIComponent);
+  Sqrl.defineFilter('fromNow', (ts) => moment(ts).fromNow());
+
+  const userTemplate = `
+        <a href="/u/{{@this.author}}"><img srcset="/avatars/{{@this.author}}-16.png 1x, /avatars/{{@this.author}}-32.png 2x" class="avatar" alt="{{@this.author}} avatar" src="/avatars/{{@this.author}}-16.png" width="16" height="16"></a>
+        authored by
+        <a href="/u/{{@this.author}}" class="u-author h-card user_is_author">{{@this.author}}</a>`;
+  Sqrl.definePartial('user', userTemplate);
+
+  const template = `
+{{each(options.json.items)}}
+    <li id="story_{{@this.id}}" data-shortid="{{@this.id}}" class="story">
+      <div class="story_liner h-entry">
+        <div class="voters">
+          <a class="upvoter" href="/login"></a>
+          <div class="score">{{@this.score}}</div>
+        </div>
+        <div class="details">
+          <span class="link h-cite u-repost-of">
+            <a class="u-url" href="{{@this.url}}">{{@this.title}}</a>
+          </span>
+          <span class="tags">
+{{each(@this.tags)}}
+            <a class="tag tag_programming" title="Use when every tag or no specific tag applies" href="/t/{{@this}}">{{@this}}</a>
+{{/each}}
+            <a class="tag tag_pdf tag_is_media" title="Link to a PDF document" href="/t/pdf">pdf</a>
+            <a class="tag tag_ask tag_is_media" title="Ask Lobsters" href="/t/ask">ask</a>
+          </span>
+          <a class="domain" href="/search?order=newest&amp;q=domain:{{@this.domain}}">{{@this.domain}}</a>
+          <div class="byline">
+            {{include(user)/}}
+            <span title="{{@this.ts}}" class="">{{@this.ts | fromNow }}</span>
+            |
+            <a href="https://archive.is/{{@this.url | encodeURIComponent}}" rel="nofollow" target="_blank">cached</a>
+            <span class="comments_label">
+              |
+              <a href="/s/{{@this.id}}/{{@this.slug}}">{{@this.comment_count}} comments</a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <a href="/s/{{@this.id}}/{{@this.slug}}" class="mobile_comments " style="display: none;">
+        <span>{{@this.comment_count}}</span>
+      </a>
+    </li>
+{{/each}}
+`;
+
+  function render(json) {
+    let content = Sqrl.Render(template, {json});
+    console.log('rendered', content);
+    return content;
+  }
+
+  class MylobItems extends HTMLElement {
+    static get observedAttributes() {
+      return [];
+    }
+
+    connectedCallback() {
+      debugger;
+      this.log('connected');
+      this.render();
+    }
+
+    attributeChangedCallback(attr, oldValue, newValue) {
+      this.log('attributeChanged');
+      this.render();
+    }
+
+    render() {
+      let json = getArticles();
+      this.log('articles', json);
+      this.innerHTML = render(json);
+    }
+
+    disconnectedCallback() {
+      this.log('disconnected');
+    }
+
+    log(...args) {
+      console.log('🖼️ mylob-items', ...args);
+    }
+  }
+
+  window.customElements.define('mylob-items', MylobItems);
+
+}());
--- script.js
+++ script.js
@@ -26,58 +26,6 @@ const init = () => {
 };
 init();
 
-Sqrl.defineFilter('encodeURIComponent', encodeURIComponent);
-Sqrl.defineFilter('fromNow', (ts) => moment(ts).fromNow());
-
-const userTemplate = `
-        <a href="/u/{{@this.author}}"><img srcset="/avatars/{{@this.author}}-16.png 1x, /avatars/{{@this.author}}-32.png 2x" class="avatar" alt="{{@this.author}} avatar" src="/avatars/{{@this.author}}-16.png" width="16" height="16"></a>
-        authored by
-        <a href="/u/{{@this.author}}" class="u-author h-card user_is_author">{{@this.author}}</a>`;
-Sqrl.definePartial('user', userTemplate);
-
-const template = `
-<div id="inside">
-  <ol class="stories list">
-{{each(options.json.items)}}
-    <li id="story_{{@this.id}}" data-shortid="{{@this.id}}" class="story">
-      <div class="story_liner h-entry">
-        <div class="voters">
-          <a class="upvoter" href="/login"></a>
-          <div class="score">{{@this.score}}</div>
-        </div>
-        <div class="details">
-          <span class="link h-cite u-repost-of">
-            <a class="u-url" href="{{@this.url}}">{{@this.title}}</a>
-          </span>
-          <span class="tags">
-{{each(@this.tags)}}
-            <a class="tag tag_programming" title="Use when every tag or no specific tag applies" href="/t/{{@this}}">{{@this}}</a>
-{{/each}}
-            <a class="tag tag_pdf tag_is_media" title="Link to a PDF document" href="/t/pdf">pdf</a>
-            <a class="tag tag_ask tag_is_media" title="Ask Lobsters" href="/t/ask">ask</a>
-          </span>
-          <a class="domain" href="/search?order=newest&amp;q=domain:{{@this.domain}}">{{@this.domain}}</a>
-          <div class="byline">
-            {{include(user)/}}
-            <span title="{{@this.ts}}" class="">{{@this.ts | fromNow }}</span>
-            |
-            <a href="https://archive.is/{{@this.url | encodeURIComponent}}" rel="nofollow" target="_blank">cached</a>
-            <span class="comments_label">
-              |
-              <a href="/s/{{@this.id}}/{{@this.slug}}">{{@this.comment_count}} comments</a>
-            </span>
-          </div>
-        </div>
-      </div>
-      <a href="/s/{{@this.id}}/{{@this.slug}}" class="mobile_comments " style="display: none;">
-        <span>{{@this.comment_count}}</span>
-      </a>
-    </li>
-{{/each}}
-  </ol>
-</div>
-`;
-
 const toolbarhtml = `
 <!DOCTYPE html>
 <html lang="en">
@@ -87,6 +35,10 @@ const toolbarhtml = `
     <meta name="HandheldFriendly" content="true" />
     <title>My Lobsters</title>
     <link rel="stylesheet" href="./style.css">
+    <script src="https://unpkg.com/squirrelly@latest/dist/squirrelly.min.js"></script>
+    <script src="https://momentjs.com/downloads/moment.min.js"></script>
+    <script src="./api/articles.js"></script>
+    <script src="../components/lob_item.js"></script>
   </head>
   <body>
     <form onsubmit="next(); return false;">
@@ -94,7 +46,13 @@ const toolbarhtml = `
       <input type="text" id="url" value="https://google.com"/>
       <button onclick="add(); return false;">add</button>
     </form>
-    <div id="wrapper"></div>
+    <div id="wrapper">
+      <div id="inside">
+        <ol class="stories list">
+          <mylob-items></mylob-items>
+        </ol>
+      </div>
+    </div>
     <element-details>
       <span slot="element-name">template</span>
       <span slot="description">A mechanism for holding client-
@@ -108,7 +66,6 @@ const toolbarhtml = `
         let url = document.querySelector('#url').value;
         window.opener.addUrlOpen(url, title)
       }
-      document.getElementById('wrapper').innerHTML = window.opener.render();
       document.head.appendChild(window.opener.createParentStyle());
       const setupOpenLink = () => {
         const aTags = document.querySelectorAll('a.u-url');
@@ -194,11 +151,4 @@ var createParentStyle = () => {
   const style = document.createElement('style');
   style.innerHTML = document.querySelector('style').innerHTML;
   return style;
-};
-
-var render = () => {
-  let json = getArticles();
-  //console.log('called', json);
-
-  return Sqrl.Render(template, {json});
 };
