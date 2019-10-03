1fdfa6fcd8dc2dd38961897e6d3f43da93fee64e
shimatani <shimatani@tgl.co.jp>
2019-10-03 15:13:24 +0900

    fix style

--- script.js
+++ script.js
@@ -8,17 +8,17 @@
       document.querySelector('#' + key + 'label')[attr] = val;
     };
   };
-  
+
   const initFromLocalStorage = () => {
     loadConf('innerText')('json');
   };
-  
+
   const init = () => {
     initEruda();
     cloneOrPullRepo();
   };
   init();
-  
+
   const commit = async jsonStr => {
     await pfs.writeFile(`${dir}/storage.json`, jsonStr, 'utf8');
     await git.add({ dir, filepath: 'storage.json' });
@@ -26,7 +26,7 @@
     localStorage.setItem('author', author);
     let mail = document.querySelector('#mail').value;
     localStorage.setItem('mail', mail);
-  
+
     let option = gitOption();
     option.message = 'update storage.json.';
     option.author = {
@@ -37,14 +37,14 @@
     await git.push(gitOption());
     console.log('push finished');
   };
-  
+
   var addUrlOpen = (url, title) => {
     console.log('url', url);
     let json = JSON.parse(localStorage.getItem('json'));
     json.items.push({ title, url });
     localStorage.setItem('json', JSON.stringify(json) + '\n');
     commit(JSON.stringify(json, null, '  '));
-  
+
     initFromLocalStorage();
     openUrl(url);
   };
