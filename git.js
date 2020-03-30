052192f8da99455a942e4e6714dcb58a0e4b3ab4
peccu <peccul@gmail.com>
2020-03-30 10:42:02 +0900

    fix indent

--- git.js
+++ git.js
@@ -1,76 +1,76 @@
-  // localstorage - fs - git
-  // clone, pull, add, commit, push
+// localstorage - fs - git
+// clone, pull, add, commit, push
 
-  const initGit = () => {
-    // window.fs = new LightningFS('fs', { wipe: true });
-    window.fs = new LightningFS('fs');
-    window.git.plugins.set('fs', window.fs);
-    window.pfs = window.fs.promises;
-    window.dir = '/storage';
+const initGit = () => {
+  // window.fs = new LightningFS('fs', { wipe: true });
+  window.fs = new LightningFS('fs');
+  window.git.plugins.set('fs', window.fs);
+  window.pfs = window.fs.promises;
+  window.dir = '/storage';
+};
+
+const gitOption = () => {
+  let option = {
+    dir: window.dir,
+    corsProxy: 'https://cors.isomorphic-git.org'
   };
+  let repo = localStorage.getItem('repo');
+  if (repo != '') {
+    option.url = repo;
+  }
+  let token = localStorage.getItem('token');
+  if (token != '') {
+    option.token = token;
+  }
+  return option;
+};
 
-  const gitOption = () => {
-    let option = {
-      dir: window.dir,
-      corsProxy: 'https://cors.isomorphic-git.org'
-    };
-    let repo = localStorage.getItem('repo');
-    if (repo != '') {
-      option.url = repo;
-    }
-    let token = localStorage.getItem('token');
-    if (token != '') {
-      option.token = token;
-    }
-    return option;
-  };
+const clone = async () => {
+  let option = gitOption();
+  option.ref = 'master';
+  option.singleBranch = true;
+  option.depth = 1;
+  await window.git.clone(option);
+};
 
-  const clone = async () => {
+export const cloneOrPullRepo = async (cb) => {
+  if(!window.dir){
+    initGit();
+  }
+  try {
+    await window.pfs.lstat(`${window.dir}/.git`);
+    console.log('now pull');
     let option = gitOption();
-    option.ref = 'master';
-    option.singleBranch = true;
-    option.depth = 1;
-    await window.git.clone(option);
-  };
-
-  export const cloneOrPullRepo = async (cb) => {
-    if(!window.dir){
-      initGit();
-    }
+    await window.git.pull(option);
+  }catch(err){
+    // console.log(err);
     try {
-      await window.pfs.lstat(`${window.dir}/.git`);
-      console.log('now pull');
-      let option = gitOption();
-      await window.git.pull(option);
-    }catch(err){
-      // console.log(err);
-      try {
-        await window.pfs.lstat(window.dir);
-      } catch (err2) {
-        await window.pfs.mkdir(window.dir);
-        console.log('made dir');
-      }
-      console.log('clone');
-      try {
-        await clone();
-      }catch(err){
-        console.log('cant clone', err);
-        repoConf();
-      }
+      await window.pfs.lstat(window.dir);
+    } catch (err2) {
+      await window.pfs.mkdir(window.dir);
+      console.log('made dir');
     }
-    cb();
-  };
+    console.log('clone');
+    try {
+      await clone();
+    }catch(err){
+      console.log('cant clone', err);
+      repoConf();
+    }
+  }
+  cb();
+};
 
-  export const repoConf = () => {
-    const script = document.createElement('script');
-    script.type = 'module';
-    script.src = './components/git_conf.js';
-    script.onload= () => {
-      const custom = document.createElement('git-config');
-      document.body.appendChild(custom);
-    };
-    document.body.appendChild(script);
+export const repoConf = () => {
+  const script = document.createElement('script');
+  script.type = 'module';
+  script.src = './components/git_conf.js';
+  script.onload= () => {
+    const custom = document.createElement('git-config');
+    document.body.appendChild(custom);
   };
+  document.body.appendChild(script);
+};
 
-  // window.repoConf = repoConf;
-  // window.cloneOrPullRepo = cloneOrPullRepo;
+// window.repoConf = repoConf;
+// window.cloneOrPullRepo = cloneOrPullRepo;
--- script.js
+++ script.js
@@ -8,56 +8,56 @@ const initButtons = () => {
   });
   document.getElementById('repoConf').addEventListener('click', repoConf);
 
-}
+};
 
-  const loadConf = attr => {
-    return key => {
-      const val = localStorage.getItem(key);
-      if (!val) {
-        return;
-      }
-      document.querySelector('#' + key + 'label')[attr] = val;
-    };
+const loadConf = attr => {
+  return key => {
+    const val = localStorage.getItem(key);
+    if (!val) {
+      return;
+    }
+    document.querySelector('#' + key + 'label')[attr] = val;
   };
+};
 
-  const initFromLocalStorage = () => {
-    loadConf('innerText')('json');
+const initFromLocalStorage = () => {
+  loadConf('innerText')('json');
+};
+
+const init = () => {
+  initButtons();
+  initEruda();
+  update(initFromLocalStorage);
+};
+init();
+
+const commit = async jsonStr => {
+  await pfs.writeFile(`${dir}/storage.json`, jsonStr, 'utf8');
+  await git.add({ dir, filepath: 'storage.json' });
+  let author = document.querySelector('#author').value;
+  localStorage.setItem('author', author);
+  let mail = document.querySelector('#mail').value;
+  localStorage.setItem('mail', mail);
+
+  let option = gitOption();
+  option.message = 'update storage.json.';
+  option.author = {
+    name: author,
+    email: mail
   };
+  let sha = await git.commit(option);
+  await git.push(gitOption());
+  console.log('push finished');
+};
 
-  const init = () => {
-    initButtons();
-    initEruda();
-    update(initFromLocalStorage);
-  };
-  init();
+var addUrlOpen = (url, title) => {
+  console.log('url', url);
+  let json = JSON.parse(localStorage.getItem('json'));
+  json.items.push({ title, url });
+  localStorage.setItem('json', JSON.stringify(json) + '\n');
+  commit(JSON.stringify(json, null, '  '));
 
-  const commit = async jsonStr => {
-    await pfs.writeFile(`${dir}/storage.json`, jsonStr, 'utf8');
-    await git.add({ dir, filepath: 'storage.json' });
-    let author = document.querySelector('#author').value;
-    localStorage.setItem('author', author);
-    let mail = document.querySelector('#mail').value;
-    localStorage.setItem('mail', mail);
-
-    let option = gitOption();
-    option.message = 'update storage.json.';
-    option.author = {
-      name: author,
-      email: mail
-    };
-    let sha = await git.commit(option);
-    await git.push(gitOption());
-    console.log('push finished');
-  };
-
-  var addUrlOpen = (url, title) => {
-    console.log('url', url);
-    let json = JSON.parse(localStorage.getItem('json'));
-    json.items.push({ title, url });
-    localStorage.setItem('json', JSON.stringify(json) + '\n');
-    commit(JSON.stringify(json, null, '  '));
-
-    initFromLocalStorage();
-    openUrl(url);
-  };
-  // window.addUrlOpen = addUrlOpen;
+  initFromLocalStorage();
+  openUrl(url);
+};
+// window.addUrlOpen = addUrlOpen;
